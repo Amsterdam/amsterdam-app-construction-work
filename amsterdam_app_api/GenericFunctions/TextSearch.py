c31a784223d76a12b80ce72e36652988cfa393b4
robert <wS8Fg3zb>
2022-03-15 13:47:39 +0100

    Character filter
    only allow characters from the regex: r"^[A-Za-zÀ-ÖØ-öø-ÿ0-9 ]+$"

--- amsterdam_app_api/GenericFunctions/TextSearch.py
+++ amsterdam_app_api/GenericFunctions/TextSearch.py
@@ -1,3 +1,4 @@
+import re
 from math import ceil
 from django.contrib.postgres.search import TrigramSimilarity, TrigramWordSimilarity, TrigramWordDistance, TrigramDistance
 from amsterdam_app_api.api_messages import Messages
@@ -14,9 +15,17 @@ class TextSearch:
         self.threshold = threshold
         self.page_size = page_size
         self.page = page
+        self.pages = 0
+        self.result = []
         self.algorithm = algorithm
 
     def search(self):
+        # Character Filter
+        pattern = r"^[A-Za-zÀ-ÖØ-öø-ÿ0-9 ]+$"
+        match = re.findall(pattern, self.query)
+        if len(match) == 0:
+            return {'page': self.result, 'pages': self.pages}
+
         # Get appropriate model and filter
         model_fields = [x.name for x in self.model._meta.get_fields() if x.name != 'data'] + ['score']
 
@@ -39,22 +48,21 @@ class TextSearch:
         objects = self.model.objects.annotate(score=score).filter(score__gte=self.threshold).order_by('-score')
         sorted_objects = list(objects)
         page = sorted_objects[self.page * self.page_size:self.page * self.page_size + self.page_size]
-        pages = int(ceil(len(sorted_objects) / float(self.page_size)))
+        self.pages = int(ceil(len(sorted_objects) / float(self.page_size)))
 
         # Build field list for given model
         if self.return_fields is not None:
             model_fields = [x for x in model_fields if x in self.return_fields] + ['score']
 
         # Filter field from search_results
-        result = list()
         for item in page:
             data = {}
             for model_field in model_fields:
                 data[model_field] = getattr(item, model_field)
-            result.append(data)
+            self.result.append(data)
 
         # Return result
-        return {'page': result, 'pages': pages}
+        return {'page': self.result, 'pages': self.pages}
 
 
 if __name__ == '__main__':
