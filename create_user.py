31263ab2c6a605cffeb1fe0b4a185e8fc6f83bf0
robert <robert@masikh.org>
2022-08-29 10:37:51 +0200

    Create team-account for web-access on backend(s)

--- create_user.py
+++ create_user.py
@@ -5,30 +5,7 @@ import django
 django.setup()
 
 
-def source_environment():
-    cwd = os.getcwd()
-    env_file = os.path.join(cwd, 'env')
-    if os.path.isfile(env_file):
-        with open(env_file, 'r') as f:
-            try:
-                lines = f.readlines()
-                for line in lines:
-                    line = line.replace('\n', '')
-                    pair = line.split('=')
-                    key = line.split('=')[0]
-                    value = "".join([pair[i] for i in range(1, len(pair))])
-                    os.environ[key] = value
-            except Exception as error:
-                print('Caught error in reading enviroment file: {error}'.format(error=error))
-                exit(False)
-    else:
-        print('No environment file found: {env_file}. Hint: set_env.py'.format(env_file=env_file))
-        exit(False)
-
-
 def create_users():
-    source_environment()
-
     model = get_user_model()
     webuser = os.getenv('WEB_USERNAME')
     webpass = os.getenv('WEB_PASSWORD')
