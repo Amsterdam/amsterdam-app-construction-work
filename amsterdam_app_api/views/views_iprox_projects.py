c799b9bb4a20cf79698e6ba196cc1bdc8effc59f
robert <robert@masikh.org>
2023-05-01 13:47:17 +0200

    Memoization removes from main

--- amsterdam_app_api/views/views_iprox_projects.py
+++ amsterdam_app_api/views/views_iprox_projects.py
@@ -84,7 +84,7 @@ def projects(request):
     Get a list of all projects. Narrow down by query param: project-type
     """
 
-    @memoize
+    # @memoize
     def _fetch_projects(deviceid):
         deviceid = request.META.get('HTTP_DEVICEID', None)
         if deviceid is None:
@@ -109,8 +109,7 @@ def projects(request):
                 lat = data['results'][0]['centroid'][1]
 
         fields = [] if model_items is None else model_items.split(',')
-        if articles_max_age is not None and 'identifier' not in fields:
-            fields.append('identifier')
+        fields.append('identifier')
 
         followed = False
         if 'followed' in fields:
