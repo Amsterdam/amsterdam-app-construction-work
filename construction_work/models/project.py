e7c3a654ab3cb3830703d328b0b60fda703fcb18
Stan Syfert <stan@west.nl>
2023-09-13 17:14:44 +0200

    Merge ProjectDetail model into Project model

--- construction_work/models/project.py
+++ construction_work/models/project.py
@@ -89,12 +89,7 @@ class Project(models.Model):
     )
     active = models.BooleanField(default=True, blank=True, db_index=True)
     source_url = models.CharField(max_length=1000, blank=False)
-    
-    # TODO: remove? not used anymore?
-    # project_type = models.CharField(max_length=40, blank=False, default="")
-    # TODO: retrieve name with id from enum?
-    # district_name = models.CharField(max_length=1000, blank=True, default="")
-    
+
     title = models.CharField(max_length=1000, blank=True, default="", db_index=True)
     subtitle = models.CharField(max_length=1000, null=True, db_index=True)
     district_id = models.IntegerField(default=None)
@@ -103,15 +98,19 @@ class Project(models.Model):
     coordinates = models.JSONField(null=True, default=dict)
     images = models.JSONField(null=True, default=list)
 
-    # TODO: deprecated
-    # content_text = models.TextField()
-    
     publication_date = models.DateField(default=None)
     modification_date = models.DateField(default=None)
     last_seen = models.DateTimeField(null=True, blank=True)
 
-    # TODO: setup relation with Article model
     news = models.JSONField(null=True, default=list)
+    # TODO: setup relation with Article model?
+    # news = models.ManyToManyField(Article)
+
+    page_id = models.IntegerField(default=None)
+    url = models.CharField(max_length=1000, blank=True, default="")
+    rel_url = models.CharField(max_length=1000, blank=True, default="")
+
+    contacts = models.JSONField(null=True, default=list)
 
     class Meta:
         ordering = ["title"]
@@ -119,31 +118,3 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         self.last_seen = datetime.now()
         super(Project, self).save(*args, **kwargs)
-
-
-class ProjectDetail(models.Model):
-    """ProjectsDetail db model
-
-    Note on 'identifier': (fields.W342) Setting unique=True on a ForeignKey has the same effect as using a
-    OneToOneField. ForeignKey(unique=True) is usually better served by a OneToOneField.
-
-    Note on 'on_delete=Models.CASCADE' When the referenced object is deleted, also delete the objects that have
-    references to it (when you remove a Project for instance, you might want to delete ProjectDetails as well). SQL
-    equivalent: CASCADE.
-    """
-
-    page_id = models.IntegerField(default=-1)
-    title = models.CharField(max_length=1000, blank=True, default="", db_index=True)
-    subtitle = models.CharField(max_length=1000, null=True, db_index=True)
-    rel_url = models.CharField(max_length=1000, blank=True, default="")
-    url = models.CharField(max_length=1000, blank=True, default="")
-    last_seen = models.DateTimeField(null=True, blank=True)
-    active = models.BooleanField(default=True, blank=True, db_index=True)
-    contacts = models.JSONField(null=True, default=list)
-
-    class Meta:
-        ordering = ["district_id", "title"]
-
-    def save(self, *args, **kwargs):
-        self.last_seen = datetime.now()
-        super(ProjectDetail, self).save(*args, **kwargs)
