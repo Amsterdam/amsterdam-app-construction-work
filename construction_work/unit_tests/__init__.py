bd2e43e23b11014a1bbac7c4ca2140d829856615
Stan Syfert <stan@west.nl>
2023-10-16 16:30:57 +0200

    Remove alt testing setup
    Not creating desired setup

--- construction_work/unit_tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from django import setup
-from django.core.management import call_command
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_application.testing_settings")
-setup()
-
-call_command('migrate')
