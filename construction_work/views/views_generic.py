58f6fbe4c565f41c2afa50ce65b83e87a4e20996
Stan Syfert <stan@west.nl>
2023-10-24 17:59:13 +0200

    Remove unused views

--- construction_work/views/views_generic.py
+++ construction_work/views/views_generic.py
@@ -24,25 +24,3 @@ def image(request):
     if image_object is not None:
         return HttpResponse(image_object.data, content_type=image_object.mime_type, status=200)
     return Response("Error: file not found", status=404)
-
-
-@swagger_auto_schema(**as_asset)
-@api_view(["GET"])
-def asset(request):
-    """Request asset from API by identifier (e.g. pdf document)"""
-    identifier = request.GET.get("id", None)
-    if identifier is None:
-        return Response({"status": False, "result": message.invalid_query}, status=422)
-
-    asset_object = Asset.objects.filter(pk=identifier).first()
-    if asset_object is not None:
-        return HttpResponse(asset_object.data, content_type=asset_object.mime_type, status=200)
-    return Response("Error: file not found", status=404)
-
-
-@swagger_auto_schema(**as_districts)
-@api_view(["GET"])
-def districts(request):
-    """Get district data"""
-    districts_data = StaticData.districts()
-    return Response({"status": True, "result": districts_data}, status=200)
