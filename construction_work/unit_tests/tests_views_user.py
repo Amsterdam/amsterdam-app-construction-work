204908eeaae8bf0aafb530b1f4cf6234f75643e2
Stan Syfert <stan@west.nl>
2023-11-01 16:32:00 +0100

    Refresh user password tests

--- construction_work/unit_tests/tests_views_user.py
+++ construction_work/unit_tests/tests_views_user.py
@@ -4,11 +4,6 @@ from django.test import TestCase
 
 from construction_work.api_messages import Messages
 
-username = "mock"
-password = "unsave"
-email = "mock@localhost"
-
-
 messages = Messages()
 
 
@@ -17,47 +12,53 @@ class SignInTest(TestCase):
 
     def setUp(self):
         """Setup test db"""
-        self.user = get_user_model().objects.create_user(username=username, password=password, email=email)
+        self.username = "mock"
+        self.password = "unsave"
+        self.email = "mock@localhost"
+
+        self.user = get_user_model().objects.create_user(username=self.username, password=self.password, email=self.email)
         self.user.save()
-        response = self.client.post("/api/v1/get-token/", {"username": username, "password": password})
+        response = self.client.post("/api/v1/get-token/", {"username": self.username, "password": self.password})
         self.headers = {"Accept": "application/json", "AUTHORIZATION": response.data["access"]}
+
+        self.api_url = "/api/v1/user/password"
 
     def tearDown(self):
         """Run teardown setup"""
-        self.user = get_user_model().objects.filter(username=username).first()
+        self.user = get_user_model().objects.filter(username=self.username).first()
         self.user.delete()
 
     def test_change_password_valid(self):
         """Test valid password"""
         payload = {
-            "username": username,
-            "old_password": password,
+            "username": self.username,
+            "old_password": self.password,
             "password": "012345678",
             "password_verify": "012345678",
         }
 
-        response = self.client.post("/api/v1/user/password", payload, headers=self.headers)
+        response = self.client.post(self.api_url, payload, headers=self.headers)
         self.assertEqual(response.data, {"status": True, "result": "password updated"})
         self.assertEqual(response.status_code, 200)
 
     def test_missing_parameter(self):
         """test missing parameter"""
-        payload = {"old_password": password, "password": "012345678", "password_verify": "012345678"}
+        payload = {"old_password": self.password, "password": "012345678", "password_verify": "012345678"}
 
-        response = self.client.post("/api/v1/user/password", payload, headers=self.headers)
+        response = self.client.post(self.api_url, payload, headers=self.headers)
         self.assertEqual(response.data, {"status": False, "result": messages.invalid_query})
         self.assertEqual(response.status_code, 422)
 
     def test_password_do_not_match(self):
         """Test non-matching passwords"""
         payload = {
-            "username": username,
-            "old_password": password,
+            "username": self.username,
+            "old_password": self.password,
             "password": "012345678",
             "password_verify": "87654321",
         }
 
-        response = self.client.post("/api/v1/user/password", payload, headers=self.headers)
+        response = self.client.post(self.api_url, payload, headers=self.headers)
         self.assertEqual(response.data, {"status": False, "result": messages.do_not_match})
         self.assertEqual(response.status_code, 401)
 
@@ -65,24 +66,24 @@ class SignInTest(TestCase):
         """test invalid username"""
         payload = {
             "username": "does_not_exist",
-            "old_password": password,
+            "old_password": self.password,
             "password": "012345678",
             "password_verify": "012345678",
         }
 
-        response = self.client.post("/api/v1/user/password", payload, headers=self.headers)
+        response = self.client.post(self.api_url, payload, headers=self.headers)
         self.assertEqual(response.data, {"status": False, "result": messages.invalid_username_or_password})
         self.assertEqual(response.status_code, 401)
 
     def test_password_invalid_password(self):
         """test invalid password"""
         payload = {
-            "username": username,
+            "username": self.username,
             "old_password": "invalid_password",
             "password": "012345678",
             "password_verify": "012345678",
         }
 
-        response = self.client.post("/api/v1/user/password", payload, headers=self.headers)
+        response = self.client.post(self.api_url, payload, headers=self.headers)
         self.assertEqual(response.data, {"status": False, "result": messages.invalid_username_or_password})
         self.assertEqual(response.status_code, 401)
