2b8a5ad3a11c86a9e7f3b1fed9ca3fe4bb30b5dc
Stan Syfert <stan@west.nl>
2023-11-01 16:41:49 +0100

    Return 'swagger_auto_schema' in docs

--- construction_work/swagger/swagger_views_devices.py
+++ construction_work/swagger/swagger_views_devices.py
@@ -5,20 +5,27 @@
 from drf_yasg import openapi
 
 from construction_work.api_messages import Messages
-from construction_work.swagger.swagger_abstract_objects import header_device_authorization, header_device_id
+from construction_work.swagger.swagger_abstract_objects import (
+    header_device_authorization,
+    header_device_id,
+)
 
 message = Messages()
 
 
 as_device_register_post = {
-    # /api/v1/device/register
+    # /api/v1/device/register swagger_auto_schema
     "methods": ["POST"],
     "manual_parameters": [header_device_authorization, header_device_id],
     "request_body": openapi.Schema(
         type=openapi.TYPE_OBJECT,
         properties={
-            "firebase_token": openapi.Schema(type=openapi.TYPE_STRING, description="firebase_token"),
-            "os": openapi.Schema(type=openapi.TYPE_STRING, description="os [ios|android]"),
+            "firebase_token": openapi.Schema(
+                type=openapi.TYPE_STRING, description="firebase_token"
+            ),
+            "os": openapi.Schema(
+                type=openapi.TYPE_STRING, description="os [ios|android]"
+            ),
         },
     ),
     "responses": {
@@ -40,7 +47,7 @@ as_device_register_post = {
 
 
 as_device_register_delete = {
-    # /api/v1/device/register
+    # /api/v1/device/register swagger_auto_schema
     "methods": ["DELETE"],
     "manual_parameters": [header_device_authorization, header_device_id],
     "responses": {
