dffb4d7fd0beff808dd61c681b5ea93cc42d77e9
Stan Syfert <stan@west.nl>
2023-11-22 14:59:22 +0100

    Fix send notification tests

--- construction_work/unit_tests/tests_send_notification.py
+++ construction_work/unit_tests/tests_send_notification.py
@@ -7,49 +7,41 @@ from unittest.mock import call, patch
 from django.test import TestCase
 
 from construction_work.api_messages import Messages
-from construction_work.models import Article, Notification, Project, ProjectManager, WarningMessage
+from construction_work.models import (
+    Article,
+    Notification,
+    Project,
+    ProjectManager,
+    WarningMessage,
+)
 from construction_work.models.device import Device
 from construction_work.push_notifications.send_notification import NotificationService
 from construction_work.unit_tests.mock_data import TestData
-from construction_work.unit_tests.mock_functions import firebase_admin_messaging_send_multicast
+from construction_work.unit_tests.mock_functions import (
+    firebase_admin_messaging_send_multicast,
+)
 
 messages = Messages()
 
 
+# TODO: actually test notification service methods
 class TestSendNotification(TestCase):
     """unit_tests"""
 
-    def __init__(self, *args, **kwargs):
-        super(TestSendNotification, self).__init__(*args, **kwargs)
-        self.data = TestData()
-
-    def tearDown(self):
-        """Remove test db items"""
-        pass  # pylint: disable=unnecessary-pass
-
     def setUp(self):
         """Setup test db"""
-        Project.objects.all().delete()
+        self.data = TestData()
+
         projects = []
         for project in self.data.projects:
             new_project = Project.objects.create(**project)
             projects.append(new_project)
 
-        ProjectManager.objects.all().delete()
         project_managers = []
         for project_manager in self.data.project_managers:
             new_project_manager = ProjectManager.objects.create(**project_manager)
             project_managers.append(new_project_manager)
 
-        # self.data.articles[0]["project_identifier"] = Project.objects.filter(
-        #     pk=self.data.articles[0]["foreign_id"]
-        # ).first()
-        news = Article.objects.create(**self.data.articles[0])
-
-        [news.projects.add(x) for x in projects]
-        news.save()
-
-        self.news_identifier = news.id
         warning_message_data1 = {
             "title": "title",
             "project": projects[0],
@@ -63,7 +55,6 @@ class TestSendNotification(TestCase):
             "project_manager": project_managers[1],
             "body": {"preface": "short text", "content": "long text"},
         }
-        WarningMessage.objects.all().delete()
 
         warning_message1 = WarningMessage.objects.create(**warning_message_data1)
         warning_message2 = WarningMessage.objects.create(**warning_message_data2)
@@ -78,26 +69,20 @@ class TestSendNotification(TestCase):
             "body": "text",
             "warning": warning_message2,
         }
-        notification_message_data3 = {
-            "title": "title",
-            "body": "text",
-            "news_identifier": self.news_identifier,
-        }
 
-        notification1 = Notification.objects.create(**notification_message_data1)
-        notification2 = Notification.objects.create(**notification_message_data2)
+        self.notification1 = Notification.objects.create(**notification_message_data1)
+        self.notification2 = Notification.objects.create(**notification_message_data2)
 
-        # TODO: We moeten ook een article (former news) kunnen hangen aan een push-notificatie... Oepsie...
-        notification3 = Notification.objects.create(**notification_message_data3)
-
-        self.notification_id_1 = str(notification1.id)
-        self.notification_id_2 = str(notification2.id)
-        self.notification_id_3 = str(notification3.id)
-
-        Device.objects.all().delete()
         for device in self.data.devices:
             new_device = Device.objects.create(**device)
             new_device.followed_projects.add(projects[0])
+
+    def tearDown(self):
+        """Remove test db items"""
+        Project.objects.all().delete()
+        ProjectManager.objects.all().delete()
+        WarningMessage.objects.all().delete()
+        Device.objects.all().delete()
 
     @patch(
         "firebase_admin.messaging.send_each_for_multicast",
@@ -110,66 +95,17 @@ class TestSendNotification(TestCase):
         debug = os.environ.get("DEBUG", "")
         os.environ["DEBUG"] = "false"
 
-        mock_logging = logging.getLogger("construction_work.generic_functions.generic_logger")
+        mock_logging = logging.getLogger(
+            "construction_work.generic_functions.generic_logger"
+        )
         with patch.object(mock_logging, "error") as mocked_log:
-            send_notification = NotificationService(self.notification_id_1)
+            send_notification = NotificationService(self.notification1)
+            send_notification.setup()
             send_notification.send_multicast_and_handle_errors()
 
-            assert mocked_log.call_args_list == [call("List of tokens that caused failures: ['foobar_token1']")]
-            self.assertEqual(len(send_notification.subscribed_device_batches), 1)
-            self.assertEqual(len(send_notification.subscribed_device_batches[0]), 2)
-
-        # reset environment
-        os.environ["DEBUG"] = debug
-
-    @patch(
-        "firebase_admin.messaging.send_each_for_multicast",
-        side_effect=firebase_admin_messaging_send_multicast,
-    )
-    def test_send_unknown_notification(self, _firebase_admin_messaging_send_multicast):
-        """First message should always fail in the mocked send_multicast mock!!!
-        Send non-existing notification
-        """
-        debug = os.environ.get("DEBUG", "")
-        os.environ["DEBUG"] = "false"
-
-        mock_logging = logging.getLogger("construction_work.generic_functions.generic_logger")
-        with patch.object(mock_logging, "error") as mocked_log:
-            send_notification = NotificationService(str(uuid.uuid4()))
-            send_notification.send_multicast_and_handle_errors()
-
-            self.assertEqual(send_notification.firebase_notification, None)
-            self.assertEqual(send_notification.project_id, None)
-            self.assertEqual(send_notification.subscribed_device_batches, None)
-            self.assertEqual(send_notification.valid_notification, False)
-            expected_result = [
-                call(
-                    "Caught error in SendNotification.set_notification(): "
-                    "'NoneType' object has no attribute 'project_identifier_id'"
-                )
+            assert mocked_log.call_args_list == [
+                call("List of tokens that caused failures: ['foobar_token1']")
             ]
-            assert mocked_log.call_args_list == expected_result
-
-        # reset environment
-        os.environ["DEBUG"] = debug
-
-    @patch(
-        "firebase_admin.messaging.send_each_for_multicast",
-        side_effect=firebase_admin_messaging_send_multicast,
-    )
-    def test_send_news(self, _firebase_admin_messaging_send_multicast):
-        """First message should always fail in the mocked send_multicast mock!!!
-        Send news item as notification
-        """
-        debug = os.environ.get("DEBUG", "")
-        os.environ["DEBUG"] = "false"
-
-        mock_logging = logging.getLogger("construction_work.generic_functions.generic_logger")
-        with patch.object(mock_logging, "error") as mocked_log:
-            send_notification = NotificationService(self.notification_id_3)
-            send_notification.send_multicast_and_handle_errors()
-
-            assert mocked_log.call_args_list == [call("List of tokens that caused failures: ['foobar_token1']")]
             self.assertEqual(len(send_notification.subscribed_device_batches), 1)
             self.assertEqual(len(send_notification.subscribed_device_batches[0]), 2)
 
